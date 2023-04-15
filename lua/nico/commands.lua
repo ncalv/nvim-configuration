e87327ab1be2253d322c8122c5cbb2b3af85f585
nico <nic.calv@outlook.it>
2023-04-15 12:21:50 +0200

    add excecute python command

--- lua/nico/commands.lua
+++ lua/nico/commands.lua
@@ -11,3 +11,40 @@ local function print_nvim_path()
 end
 
 vim.api.nvim_create_user_command('PrintNvimPath', print_nvim_path, {})
+
+
+local function execute_current_buf_as_python()
+  local res_command = '!python -c '
+  local buf_lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+  -- escape quotes so it is a correct string for fish shell
+  local function escape_quotes(str)
+    local escaped_str = ''
+    for i = 1, string.len(str) do
+      local ch = string.sub(str, i, i)
+      if ch == '"' or ch == '\''
+      then
+        escaped_str = escaped_str .. '\\' .. ch
+      else
+        escaped_str = escaped_str .. ch
+      end
+    end
+
+    return escaped_str
+  end
+
+  for _, val in pairs(buf_lines) do
+    res_command = res_command .. '"' .. escape_quotes(val) .. '"\\n'
+  end
+
+  vim.api.nvim_command(res_command)
+end
+
+local function print_current_buf_name()
+  print(vim.api.nvim_buf_get_name(0))
+end
+
+vim.api.nvim_create_user_command('GetBufName', print_current_buf_name, {})
+
+vim.api.nvim_create_user_command('Pyrun', execute_current_buf_as_python, {})
+
